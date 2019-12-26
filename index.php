606b9b920123cd43249af142befdf0b2210173dc
nouamane lachhab <nouamanelachhab@outlook.com>
2019-12-26 09:43:22 +0100

    index.php added

---
+++ index.php
@@ -0,0 +1,32 @@
+<html> <head>   <title>Divide Tab </title> <style> body, html {  
+width: 100%;   height: 100%;   margin: 0; }
+
+.container {   width: 100%;   height: 100%; } .leftpane {
+    width: 25%;
+    height: 100%;
+    float: left;
+    background-color: rosybrown;
+    border-collapse: collapse;
+     text-align: center; }
+
+.middlepane {
+    width: 50%;
+    height: 100%;
+    float: left;
+    background-color: royalblue;
+    border-collapse: collapse;
+     text-align: center; }
+
+.rightpane {   width: 25%;   height: 100%;   position: relative;  
+float: right;   background-color: yellow;   border-collapse:
+collapse;    text-align: center; }
+
+.toppane {   width: 100%;   height: 100px;   border-collapse:
+collapse;   background-color: #4da6ff;   text-align: center; }
+
+</style>
+
+</head> <body>  <div class="container">   <div
+class="toppane"><h2>Top View</h2></div>   <div class="leftpane">
+    <h1>Left View</h1></div>   <div class="middlepane"><h2>Middle View</h2></div>   <div class="rightpane">
+    <h1>Right View</h1></div> </div> </body> </html>
